2c9ad76744bd49ecf3e8ba015cd07c85cc6b0ddd
Muhammad Haseeb <90205212+MuhammadHaseebOdho@users.noreply.github.com>
2023-06-16 18:20:00 +0500

    CityComparator.java

--- Comparator-Comparable/CityComparator.java
+++ Comparator-Comparable/CityComparator.java
@@ -1,7 +1,7 @@
 import java.util.Comparator;
 
 public class CityComparator implements Comparator<PersonComparator> {
-    @Override
+    
     public int compare(PersonComparator o1, PersonComparator o2) {
         return o1.getCity().compareTo(o2.getCity());
     }
